1435541743130219a9d9828e97f46e297aa2cd74
Jeremy Chia <jeremyjchia@gmail.com>
2024-01-21 19:59:43 +0100

    feat: creates mart_attendance

---
+++ models/mart/mart_attendance.sql
@@ -0,0 +1,58 @@
+{{ config(
+    materialized='table'
+) }}
+
+-- sources
+
+with attendance as (
+    select
+        date,
+        member_name,
+        is_present
+    from {{ ref('fact_attendance') }}
+),
+
+sittings as (
+    select
+        date,
+        parliament,
+        session,
+        volume,
+        sittings
+    from {{ ref('fact_sittings') }}
+),
+
+members as (
+    select
+        member_name,
+        party,
+        gender
+    from {{ ref('dim_members') }}
+),
+
+joined as (
+    select
+        -- metadata
+        attendance.date,
+        sittings.parliament,
+        sittings.session,
+        sittings.volume,
+        sittings.sittings,
+
+        -- member information
+        attendance.member_name,
+        members.party as member_party,
+        members.gender as member_gender,
+
+        -- attendance information
+        attendance.is_present
+
+    from attendance
+    left join sittings
+        on attendance.date = sittings.date
+    left join members
+        on attendance.member_name = members.member_name
+)
+
+select *
+from joined
