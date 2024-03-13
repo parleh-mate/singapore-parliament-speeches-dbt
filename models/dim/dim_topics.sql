1100124c0d039d8359fec7a81bba413890cad85b
jeremychia <jeremy.chia@vinted.com>
2024-03-13 08:18:32 +0800

    feat: introduce is_constitutional flag on topics

--- models/dim/dim_topics.sql
+++ models/dim/dim_topics.sql
@@ -32,6 +32,20 @@ joined as (
     from topics
     left join seed_topic_type
         on topics.section_type = seed_topic_type.section_type_code
+),
+
+flags as (
+    select
+        -- from previous cte
+        topic_id,
+        date,
+        topic_order,
+        title,
+        section_type,
+        section_type_name,
+        -- introduced in this cte
+        lower(title) like "%constitution%" and section_type in ("BI", "BP") as is_constitutional
+    from joined
 )
 
 select *
