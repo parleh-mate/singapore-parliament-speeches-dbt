07f75299318d69e777a8e38f5684d48615886bcd
jeremychia <jeremy.chia@vinted.com>
2024-03-16 11:50:13 +0800

    bug: replace chairman with speaker

--- models/stg/stg_speeches.sql
+++ models/stg/stg_speeches.sql
@@ -10,7 +10,7 @@ with type_cast as (
         cast(speech_id as string) as speech_id,
         cast(topic_id as string) as topic_id,
         cast(speech_order as int64) as speech_order,
-        cast(member_name as string) as member_name,
+        cast(case when member_name_original like '%Chairman%' then 'Speaker' else member_name end as string) as member_name,
         cast(text as string) as text,
         date(left(topic_id, 10)) as date,
         cast(num_words as int64) as count_words,
