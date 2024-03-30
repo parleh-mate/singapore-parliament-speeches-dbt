2e46a62c6904af11808733c28d061955d9cbc078
Jeremy Chia <jeremyjchia@gmail.com>
2024-03-30 21:30:37 +0100

    chore: change name

--- models/stg/stg_topic_names.sql
+++ models/stg/stg_topic_names.sql
@@ -7,7 +7,7 @@
 with type_cast as (
     select
         cast(topic as int64) as topic_group_id,
-        cast(topic_summary as string) as topic_group_summary,
+        cast(topic_summary as string) as topic_group_name,
         cast(top_n_words as string) as topic_group_top_n_words
     from {{ source('topic_modelling', 'topic_names_16_nmf_20240330') }}
 )
