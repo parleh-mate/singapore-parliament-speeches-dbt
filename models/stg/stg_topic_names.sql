735b02fa78e7050cedc505a221dc272312d1b361
Jeremy Chia <jeremyjchia@gmail.com>
2024-03-31 19:31:03 +0200

    feat: change source to new ML model

--- models/stg/stg_topic_names.sql
+++ models/stg/stg_topic_names.sql
@@ -9,7 +9,7 @@ with type_cast as (
         cast(topic as int64) as topic_group_id,
         cast(topic_summary as string) as topic_group_name,
         cast(top_n_words as string) as topic_group_top_n_words
-    from {{ source('topic_modelling', 'topic_names_16_nmf_20240330') }}
+    from {{ source('topic_modelling', 'topic_names_25_nmf_20240331') }}
 )
 
 select *
