ae3497e7d004bf6ee135e18e40f988d7b65aed0f
jeremychia <jeremy.chia@vinted.com>
2024-04-28 11:09:10 +0200

    feat: select committee dedup

--- models/stg/members_information/stg_members_select_committee.sql
+++ models/stg/members_information/stg_members_select_committee.sql
@@ -6,9 +6,38 @@ with
             {{ adapter.quote("parliament") }} as parliament,
             {{ adapter.quote("session") }} as session,
             {{ adapter.quote("committee") }} as member_committee,
-            {{ adapter.quote("position") }} as member_committe_position
+            {{ adapter.quote("position") }} as member_committee_position
 
         from source
+    ),
+    dedup as (
+        select
+            member_name,
+            parliament,
+            session,
+            member_committee,
+            member_committee_position
+        from renamed
+        qualify
+            row_number() over (
+                partition by
+                    member_name,
+                    parliament,
+                    session,
+                    member_committee,
+                    member_committee_position
+            )
+            = 1
+    ),
+    array_agg as (
+        select
+            member_name,
+            parliament,
+            session,
+            member_committee,
+            array_agg(member_committee_position) as member_committee_position
+        from dedup
+        group by all
     )
 select *
-from renamed
+from array_agg
