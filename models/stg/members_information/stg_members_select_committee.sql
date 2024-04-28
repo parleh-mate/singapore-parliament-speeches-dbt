f9e928d0d0a9eb7ce6514a9056c18cf3e83b0e87
jeremychia <jeremy.chia@vinted.com>
2024-04-28 20:43:56 +0200

    feat: add dates (only from 12th parliament)

--- models/stg/members_information/stg_members_select_committee.sql
+++ models/stg/members_information/stg_members_select_committee.sql
@@ -38,6 +38,40 @@ with
             array_agg(member_committee_position) as member_committee_position
         from dedup
         group by all
+    ),
+    -- include dates
+    max_date as (select max(date) as latest_date from {{ ref("stg_sittings") }}),
+
+    parliament_sesion_dates as (
+        select
+            parliament,
+            session,
+            case
+                when parliament = 12 and session = 1 then '2010-10-10' else min(date)  -- hardcode dates
+            end as effective_from_date,
+            case
+                when max(date) = (select latest_date from max_date)
+                then null
+                else max(date)
+            end as effective_to_date
+        from {{ ref("stg_sittings") }}
+        group by all
+    ),
+    -- join dates
+    join_dates as (
+        select
+            array_agg.member_name,
+            array_agg.parliament,
+            array_agg.session,
+            array_agg.member_committee,
+            array_agg.member_committee_position,
+            parliament_sesion_dates.effective_from_date,
+            parliament_sesion_dates.effective_to_date
+        from array_agg
+        left join
+            parliament_sesion_dates
+            on cast(array_agg.parliament as int) = parliament_sesion_dates.parliament
+            and cast(array_agg.session as int) = parliament_sesion_dates.session
     )
 select *
-from array_agg
+from join_dates
