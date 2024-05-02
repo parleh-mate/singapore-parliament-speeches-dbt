12a59d0e1081eae0f8a6463768c376ce461425da
jeremychia <jeremy.chia@vinted.com>
2024-05-02 08:17:48 +0200

    feat: union with scraped information

--- models/stg/members_information/stg_members_office_holding.sql
+++ models/stg/members_information/stg_members_office_holding.sql
@@ -4,18 +4,32 @@ with
         select
             {{ adapter.quote("member_name") }} as member_name,
             {{ adapter.quote("position") }} as member_appointment,
-            {{ adapter.quote("from_date") }} as effective_from_date,
-            {{ adapter.quote("to_date") }} as effective_to_date
-
+            cast({{ adapter.quote("from_date") }} as date) as effective_from_date,
+            cast({{ adapter.quote("to_date") }} as date) as effective_to_date
         from source
     ),
+
+    -- union manually-filled information
+    manual_gsheet as (
+        select member_name, member_appointment, effective_from_date, effective_to_date
+        from {{ ref("stg_gsheet_office_holding") }}
+    ),
+
+    unioned as (
+        select member_name, member_appointment, effective_from_date, effective_to_date
+        from renamed
+        union all
+        select member_name, member_appointment, effective_from_date, effective_to_date
+        from manual_gsheet
+    ),
+
     add_latest_flag as (
         select
             *,
             effective_to_date
             is null  -- when it is null, this is the current appointment
             as is_latest_appointment
-        from renamed
+        from unioned
     )
 select *
 from add_latest_flag
--- models/stg/members_information/stg_members_year_of_birth.sql
+++ models/stg/members_information/stg_members_year_of_birth.sql
@@ -3,9 +3,21 @@ with
     renamed as (
         select
             {{ adapter.quote("member_name") }} as member_name,
-            {{ adapter.quote("year_of_birth") }} as member_birth_year,
+            cast({{ adapter.quote("year_of_birth") }} as int) as member_birth_year,
 
         from source
+    ),
+    -- union manually-filled information
+    manual_gsheet as (
+        select member_name, member_birth_year from {{ ref("stg_gsheet_year_of_birth") }}
+    ),
+
+    unioned as (
+        select member_name, member_birth_year
+        from renamed
+        union all
+        select member_name, member_birth_year
+        from manual_gsheet
     )
 select *
-from renamed
+from unioned
