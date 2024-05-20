ee50a9ba687e357b896b522e1ccb9ffd9062b24d
jeremychia <jeremy.chia@vinted.com>
2024-05-20 19:14:56 +0200

    refactor: replace encoded different space to standardise names

--- models/stg/stg_speeches.sql
+++ models/stg/stg_speeches.sql
@@ -14,54 +14,17 @@ with
                     then 'Deputy Speaker'
                     when member_name = 'Alex Yam Ziming'
                     then 'Alex Yam'
-                    -- amy khor, but with different encoding
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '416d79204b686f72c2a04c65616e205375616e'
-                    then 'Amy Khor Lean Suan'
                     when member_name = 'David Ong Kim Huat'
                     then 'David Ong'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '44656e6973652050687561204c6179c2a050656e67'
-                    then 'Denise Phua Lay Peng'
                     when member_name = 'Dr Yaacob Ibrahim'
                     then 'Yaacob Ibrahim'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '48656e672053776565c2a04b656174'
-                    then 'Heng Swee Keat'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '496e6472616e6565c2a052616a6168'
-                    then 'Indranee Rajah'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '4972656e65204e67205068656bc2a0486f6f6e67'
-                    then 'Irene Ng Phek Hoong'
                     when member_name = 'Josephine'
                     then 'Josephine Teo'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '4c696d2042696f77c2a0436875616e'
-                    then 'Lim Biow Chuan'
                     when member_name = 'Melvin Yong'
                     then 'Melvin Yong Yik Chye'
                     when member_name = 'Mohd Fahmi Bin Aliman'
                     then 'Mohd Fahmi Aliman'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '4d7568616d61642046616973616c2042696e20416264756cc2a04d616e6170'
-                    then 'Muhamad Faisal Bin Abdul Manap'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '5269746120536f682053696f77c2a04c616e'
-                    then 'Rita Soh Siow Lan'
-                    when
-                        to_hex(cast(member_name as bytes))
-                        = '596565204a656e6ec2a04a6f6e67'
-                    then 'Yee Jenn Yong'
-                    else trim(member_name)
+                    else trim(replace(member_name, chr(160), ' ')
                 end as string
             ) as member_name,
             cast(text as string) as text,
