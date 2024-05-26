bb66acd5c54d141c15924fe273749037648dc989
jeremychia <jeremy.chia@vinted.com>
2024-05-26 12:09:43 +0200

    fix: clean members names

--- models/stg/stg_attendance.sql
+++ models/stg/stg_attendance.sql
@@ -8,7 +8,25 @@ with
             cast(is_present as bool) as is_present
         from {{ source("raw", "attendance") }}
         where member_name is not null
+    ),
+
+    standardise_member_name as (
+        select
+            * except (member_name),
+            -- mapping of names here:
+            -- https://docs.google.com/spreadsheets/d/1Wk_PDlQbWWViTV9NmDPsXwu54TD96ltEycLAKOHe1fA/edit#gid=1498942254
+            coalesce(mapping.member_name, type_cast.member_name) as member_name
+        from type_cast
+        left join
+            {{ source("google_sheets", "member_name_mapping") }} as mapping
+            on mapping.possible_variation = type_cast.member_name
+    ),
+
+    filter_empty_member_name as (
+        select *
+        from standardise_member_name
+        where member_name != ''
     )
 
 select *
-from type_cast
+from filter_empty_member_name
