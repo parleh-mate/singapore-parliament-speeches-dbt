fa9de68e5ff7eb21d318250b2e0cc5f7924c6e08
jeremychia <jeremy.chia@vinted.com>
2024-06-02 23:05:14 +0200

    feat: introduce supplementary image sources

--- models/stg/members_information/stg_members_image.sql
+++ models/stg/members_information/stg_members_image.sql
@@ -1,11 +1,22 @@
 with
     source as (select * from {{ source("raw", "members_image") }}),
+    gsheet as (select * from {{ ref("stg_gsheet_member_image_link") }}),
     renamed as (
         select
             {{ adapter.quote("member_name") }} as member_name,
             {{ adapter.quote("image_link") }} as member_image_link
-
         from source
+    ),
+    unioned as (
+        select
+            member_name,
+            member_image_link
+        from renamed
+        union all
+        select
+            member_name,
+            member_image_link
+        from gsheet
     )
 select *
-from renamed
+from unioned
