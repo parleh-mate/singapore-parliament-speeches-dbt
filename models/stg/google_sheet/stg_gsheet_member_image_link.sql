c2c5116bac900680616be1a40a67f9cd7b494eb1
jeremychia <jeremy.chia@vinted.com>
2024-06-03 08:17:16 +0200

    chore: linting

--- models/stg/google_sheet/stg_gsheet_member_image_link.sql
+++ models/stg/google_sheet/stg_gsheet_member_image_link.sql
@@ -1,11 +1,10 @@
-with source as (
-      select * from {{ source('google_sheets', 'member_image_link') }}
-),
-renamed as (
-    select
-        {{ adapter.quote("member_name") }},
-        {{ adapter.quote("member_image_link") }}
+with
+    source as (select * from {{ source("google_sheets", "member_image_link") }}),
+    renamed as (
+        select
+            {{ adapter.quote("member_name") }}, {{ adapter.quote("member_image_link") }}
 
-    from source
-)
-select * from renamed
+        from source
+    )
+select *
+from renamed
