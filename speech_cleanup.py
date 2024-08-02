e4b41331df47b8b8858644c84d782480147c2f52
jonfoong <jonahfoong@gmail.com>
2024-08-02 15:06:16 +0200

    edit prompt

--- speech_cleanup.py
+++ speech_cleanup.py
@@ -1,6 +1,5 @@
 import pandas_gbq
 import os
-import pandas as pd
 import numpy as np
 import re
 
--- speech_summaries.py
+++ speech_summaries.py
@@ -64,7 +64,7 @@ def get_summaries(speech_dat):
               "gpt-4o-mini"]
     
     speech = speech_dat.speech_text.iloc[0]
-    prompt = "You are a helpful assistant who will help to summarize speechs made in the Singapore parliament. Summaries should be short and concise, between 2-3 sentences and ideally less than 60 words, and be written in the perspective of the speaker. Opinions and arguments should be stated immediately rather than written like 'I believe...', 'I am here to...'."
+    prompt = "You are a helpful assistant who will help to summarize speeches made in the Singapore parliament. Summaries should be short and concise, between 2-3 sentences and ideally less than 60 words, and be written in the perspective of the speaker. Opinions and arguments should be stated immediately rather than written like 'I believe...', 'I am here to...'."
     df_list = []    
     for i in models:
         if "claude" in i:
