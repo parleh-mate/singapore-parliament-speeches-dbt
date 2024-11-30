02562d76476817268861115f81556b2e7aabba9e
jeremychia <38421339+jeremychia@users.noreply.github.com>
2024-11-30 13:14:25 +0100

    chore: sqlfmt linting (#39)
    * chore: sqlfmt linting
    * chore: replace with liquid referencing

--- models/dashboard/dash_demographics.sql
+++ models/dashboard/dash_demographics.sql
@@ -1,11 +1,27 @@
-with get_demographics as(SELECT member_name, parliament, party as member_party, member_constituency, member_ethnicity, gender, member_birth_year, extract(year from first_parl_date) - member_birth_year as year_age_entered
-                    FROM `singapore-parliament-speeches.prod_dim.dim_members`
-                    left join (SELECT member_name, parliament, member_constituency, min(`date`) as first_parl_date
-                        FROM `singapore-parliament-speeches.prod_mart.mart_attendance`
-                        where member_constituency is not null
-                        group by member_name, parliament, member_constituency)
-                    using (member_name)
-                    order by member_name, parliament
-)
+with
+    get_demographics as (
+        select
+            member_name,
+            parliament,
+            party as member_party,
+            member_constituency,
+            member_ethnicity,
+            gender,
+            member_birth_year,
+            extract(year from first_parl_date) - member_birth_year as year_age_entered
+        from {{ ref("dim_members") }}
+        left join
+            (
+                select
+                    member_name,
+                    parliament,
+                    member_constituency,
+                    min(`date`) as first_parl_date
+                from {{ ref("mart_attendance") }}
+                where member_constituency is not null
+                group by member_name, parliament, member_constituency
+            ) using (member_name)
+        order by member_name, parliament
+    )
 select *
 from get_demographics
--- models/dashboard/dash_questions.sql
+++ models/dashboard/dash_questions.sql
@@ -1,17 +1,41 @@
-with by_parl_questions as (SELECT member_name, cast(parliament as STRING) as parliament, member_constituency, member_party, ministry_addressed, count(*) as count_questions_ministry
-FROM `singapore-parliament-speeches.prod_mart.mart_speeches`
-where is_primary_question
-and member_party is not NULL
-and ministry_addressed is not NULL
-group by member_name, parliament, member_constituency, member_party, ministry_addressed
-),
-all_parl_questions as (SELECT member_name, 'All' as parliament, 'All' as member_constituency, member_party, ministry_addressed, count(*) as count_questions_ministry
-FROM `singapore-parliament-speeches.prod_mart.mart_speeches`
-where is_primary_question
-and member_party is not NULL
-and ministry_addressed is not NULL
-group by member_name, member_party, ministry_addressed
-)
-select * from by_parl_questions
+with
+    by_parl_questions as (
+        select
+            member_name,
+            cast(parliament as string) as parliament,
+            member_constituency,
+            member_party,
+            ministry_addressed,
+            count(*) as count_questions_ministry
+        from {{ ref("mart_speeches") }}
+        where
+            is_primary_question
+            and member_party is not null
+            and ministry_addressed is not null
+        group by
+            member_name,
+            parliament,
+            member_constituency,
+            member_party,
+            ministry_addressed
+    ),
+    all_parl_questions as (
+        select
+            member_name,
+            'All' as parliament,
+            'All' as member_constituency,
+            member_party,
+            ministry_addressed,
+            count(*) as count_questions_ministry
+        from {{ ref("mart_speeches") }}
+        where
+            is_primary_question
+            and member_party is not null
+            and ministry_addressed is not null
+        group by member_name, member_party, ministry_addressed
+    )
+select *
+from by_parl_questions
 union all
-select * from all_parl_questions
+select *
+from all_parl_questions
