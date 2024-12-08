536d574ada2d28e08147bb0426d50384cfef72e8
Jonah Foong <singapore.parliament.speeches@gmail.com>
2024-12-08 16:46:32 +0000

    omit parliamentary questions from list of speeches that are summarized

--- models/dashboard/dash_speech_lengths.sql
+++ models/dashboard/dash_speech_lengths.sql
@@ -5,8 +5,10 @@ with
         where
             topic_type_name not like "%Correction by Written Statements%"
             and topic_type_name not like "%Bill Introduced%"
+            and not is_primary_question
             and member_name != ''
             and member_name != 'Speaker'
+            and member_constituency is not NULL
     )
 select *
 from speech_lengths
