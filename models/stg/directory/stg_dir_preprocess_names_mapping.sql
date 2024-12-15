e696ca4205f12ee370b35ae1610b74cfc8ea2d32
jeremychia <jeremy.chia@vinted.com>
2024-12-15 22:13:09 +0100

    chore: migrate to singapore-government-directory/dbt

--- models/stg/directory/stg_dir_preprocess_names_mapping.sql
+++
@@ -1,9 +0,0 @@
-with
-    source as (select * from {{ source("sg_govt_dir_preprocess", "names_mapping") }}),
-    renamed as (
-        select {{ adapter.quote("extracted_name") }}, {{ adapter.quote("name") }}
-
-        from source
-    )
-select *
-from renamed
