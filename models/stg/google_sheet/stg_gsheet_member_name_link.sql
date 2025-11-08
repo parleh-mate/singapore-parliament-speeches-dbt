2289cd4afbf8c7270467845ba12976ad9b73e433
jonfoong <jonahfoong@gmail.com>
2025-11-08 16:33:14 +0100

    get member full names and website links

---
+++ models/stg/google_sheet/stg_gsheet_member_name_link.sql
@@ -0,0 +1,10 @@
+with
+    source as (select * from {{ source("google_sheets", "member_name_link") }}),
+    renamed as (
+        select
+            {{ adapter.quote("member_name") }} as member_name,
+            {{ adapter.quote("member_link_name") }} as member_name_website
+        from source
+    )
+select *
+from renamed
