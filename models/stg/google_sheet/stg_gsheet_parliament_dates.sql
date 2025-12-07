6ce96482ce4944366ac07b1fee36235607f637ef
jonfoong <jonahfoong@gmail.com>
2025-12-07 21:40:40 +0100

    materialize parliament dates table for use in other functions

--- models/stg/google_sheet/stg_gsheet_parliament_dates.sql
+++ models/stg/google_sheet/stg_gsheet_parliament_dates.sql
@@ -1,3 +1,6 @@
+-- materialize as table to allow for use without needing to expand drive scopes for other projects --
+{{ config(materialized='table') }}
+
 with
     source as (select * from {{ source("google_sheets", "parliament_dates") }})
 select *
